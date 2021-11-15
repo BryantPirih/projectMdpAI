3cf957cf71516124f4259e1ba1967482009eea5b
JonathanMarcell <jonathanmarcellatmadja.jma@gmail.com>
2021-11-15 08:35:01 +0700

    folder

--- TootCareApp/app/src/main/java/com/mdpproject/toothcareapp/UI/RegisterActivity.java
+++ TootCareApp/app/src/main/java/com/mdpproject/toothcareapp/UI/RegisterActivity.java
@@ -1,14 +1,16 @@
-package com.mdpproject.toothcareapp;
+package com.mdpproject.toothcareapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.mdpproject.toothcareapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
