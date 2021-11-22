45256c7fc0a51342fb41f3a4f1412d2f53ff13a5
joshuanando <joshuanando7@gmail.com>
2021-11-22 13:12:36 +0700

    UI Login, Register, UserHome

--- app/src/main/java/com/bryant/projectmdpai/UserFragment/UserHomeFragment.java
+++ app/src/main/java/com/bryant/projectmdpai/UserFragment/UserHomeFragment.java
@@ -1,45 +1,37 @@
 package com.bryant.projectmdpai.UserFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bryant.projectmdpai.R;
+import com.bryant.projectmdpai.databinding.FragmentUserHomeBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserHomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserHomeFragment extends Fragment {
+    private static final String ARG_PARAM_MENU = "param-menu";
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentUserHomeBinding binding;
+    private String menu;
 
     public UserHomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment UserHomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserHomeFragment newInstance() {
+    public static UserHomeFragment newInstance(String paramMenu) {
         UserHomeFragment fragment = new UserHomeFragment();
         Bundle args = new Bundle();
+        args.putString(ARG_PARAM_MENU, paramMenu);
         fragment.setArguments(args);
         return fragment;
     }
@@ -48,13 +40,24 @@ public class UserHomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+            menu = getArguments().getString(ARG_PARAM_MENU);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_home, container, false);
+        binding = FragmentUserHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.txtTitle.setText(menu);
+    }
+
+    void setUpRecyclerView(){
     }
 }
--- app/src/main/java/com/bryant/projectmdpai/UserHome.java
+++ app/src/main/java/com/bryant/projectmdpai/UserHome.java
@@ -1,14 +1,84 @@
 package com.bryant.projectmdpai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.bryant.projectmdpai.Class.User;
+import com.bryant.projectmdpai.UserFragment.UserHomeFragment;
+import com.bryant.projectmdpai.databinding.ActivityUserHomeBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class UserHome extends AppCompatActivity {
+
+    private ActivityUserHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_home);
+        binding = ActivityUserHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottNavUser.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                String menu;
+                switch (item.getItemId()){
+                    case R.id.itm_user_forum:
+                        menu = "Forum";
+                        break;
+                    case R.id.itm_user_ask:
+                        menu = "Ask Doctor";
+                        break;
+                    case R.id.itm_user_es:
+                        menu = "Expert System";
+                        break;
+                    default:
+                        menu = "Articles";
+                        break;
+                }
+
+                try {
+                    Fragment fragment = UserHomeFragment.newInstance(menu);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frameLayoutUser, fragment)
+                            .commit();
+                }catch (Exception e){
+                    Log.e("MainActivity", e.getMessage());
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.option_home:
+                Intent toHome = new Intent(getApplicationContext(), UserHome.class);
+                startActivity(toHome);
+                break;
+            case R.id.option_profile:
+                Intent toProfile = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(toProfile);
+                break;
+            default:
+                Intent toLogin = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(toLogin);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
