4ca5aa8cc6a085e3e7519bd2d39611cdc58ce6a8
joshuanando <joshuanando7@gmail.com>
2021-11-29 13:30:52 +0700

    upload image doc profile

--- app/src/main/java/com/bryant/projectmdpai/DoctorProfile.java
+++ app/src/main/java/com/bryant/projectmdpai/DoctorProfile.java
@@ -1,16 +1,28 @@
 package com.bryant.projectmdpai;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.bryant.projectmdpai.databinding.ActivityDoctorProfileBinding;
 
+import java.io.ByteArrayOutputStream;
+
 public class DoctorProfile extends AppCompatActivity {
 
     private ActivityDoctorProfileBinding binding;
+    private String Document_img1 = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +34,7 @@ public class DoctorProfile extends AppCompatActivity {
         binding.btnChangePicDocProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                selectImage();
             }
         });
 
@@ -53,5 +65,54 @@ public class DoctorProfile extends AppCompatActivity {
 
     void makeToast(String msg){
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void selectImage(){
+        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == 2){
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                thumbnail = getResizedBitmap(thumbnail, 400);
+                //Log.w("path of image from gallery......******************.........", picturePath+"");
+                binding.imgDocProfile.setImageBitmap(thumbnail);
+                BitMapToString(thumbnail);
+            }
+        }
+    }
+
+    public String BitMapToString(Bitmap thumbnail) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.PNG, 60, baos);
+        byte[] b = baos.toByteArray();
+        Document_img1 = Base64.encodeToString(b, Base64.DEFAULT);
+        return Document_img1;
+    }
+
+    private Bitmap getResizedBitmap(Bitmap thumbnail, int maxSize) {
+        int width = thumbnail.getWidth();
+        int height = thumbnail.getHeight();
+
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(thumbnail, width, height, true);
     }
 }
