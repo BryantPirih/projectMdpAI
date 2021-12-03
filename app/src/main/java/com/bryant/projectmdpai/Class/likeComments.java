2f6f558dbb5d103ecee5301ba8cb6477bf70dc04
Bryant Pirih <74889629+BryantPirih@users.noreply.github.com>
2021-12-03 15:59:51 +0700

    likeComments class

---
+++ app/src/main/java/com/bryant/projectmdpai/Class/likeComments.java
@@ -0,0 +1,70 @@
+package com.bryant.projectmdpai.Class;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class likeComments implements Parcelable {
+    private int id_article;
+    private int like;
+    private String comment;
+
+    public likeComments(int id_article, int like, String comment) {
+        this.id_article = id_article;
+        this.like = like;
+        this.comment = comment;
+    }
+
+    protected likeComments(Parcel in) {
+        id_article = in.readInt();
+        like = in.readInt();
+        comment = in.readString();
+    }
+
+    public static final Creator<likeComments> CREATOR = new Creator<likeComments>() {
+        @Override
+        public likeComments createFromParcel(Parcel in) {
+            return new likeComments(in);
+        }
+
+        @Override
+        public likeComments[] newArray(int size) {
+            return new likeComments[size];
+        }
+    };
+
+    public int getId_article() {
+        return id_article;
+    }
+
+    public void setId_article(int id_article) {
+        this.id_article = id_article;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id_article);
+        parcel.writeInt(like);
+        parcel.writeString(comment);
+    }
+}
