af27b9f41556ef2eabb84c731dc8dac7fbb195b2
joshuanando <joshuanando7@gmail.com>
2021-12-11 15:14:49 +0700

    nambah button back dari forum

--- app/src/main/java/com/bryant/projectmdpai/AddQuestionForum.java
+++ app/src/main/java/com/bryant/projectmdpai/AddQuestionForum.java
@@ -39,48 +39,48 @@ public class AddQuestionForum extends AppCompatActivity {
             System.out.println(uid);
         }
 
-        binding.btnPostQ.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String question = binding.edtQuestionForum.getText().toString();
-                String title = binding.edtTitleForum.getText().toString();
-                if(question.equals("")||title.equals("")){
-                    Toast.makeText(AddQuestionForum.this, "Isi dahulu semua field yang tersedia", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    binding.progressBarQuestionForum.setVisibility(View.VISIBLE);
-                    DatabaseReference reference = FirebaseDatabase
-                            .getInstance(getResources().getString(R.string.url_db))
-                            .getReference("questions");
-
-                    Date date = Calendar. getInstance(). getTime();
-                    DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd, hh:mm");
-                    String strDate = dateFormat. format(date);
-
-                    DatabaseReference pushedRef = reference.push();
-                    String key = pushedRef.getKey();
-                    Question q = new Question(key,uid,title,question, strDate);
-                    reference.child(key).setValue(q).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            binding.progressBarQuestionForum.setVisibility(View.GONE);
-                            Intent toHome = new Intent(getApplicationContext(), UserHome.class);
-                            toHome.putExtra("uid",uid);
-                            startActivity(toHome);
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(AddQuestionForum.this, "Question gagal di post", Toast.LENGTH_SHORT).show();
-                            binding.progressBarQuestionForum.setVisibility(View.GONE);
-                        }
-                    });
-
-
-
-                }
-
+        binding.btnPostQ.setOnClickListener(view -> {
+            String question = binding.edtQuestionForum.getText().toString();
+            String title = binding.edtTitleForum.getText().toString();
+            if(question.equals("")||title.equals("")){
+                Toast.makeText(AddQuestionForum.this, "Isi dahulu semua field yang tersedia", Toast.LENGTH_SHORT).show();
             }
+            else{
+                binding.progressBarQuestionForum.setVisibility(View.VISIBLE);
+                DatabaseReference reference = FirebaseDatabase
+                        .getInstance(getResources().getString(R.string.url_db))
+                        .getReference("questions");
+
+                Date date = Calendar. getInstance(). getTime();
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd, hh:mm");
+                String strDate = dateFormat. format(date);
+
+                DatabaseReference pushedRef = reference.push();
+                String key = pushedRef.getKey();
+                Question q = new Question(key, uid, title, question, strDate);
+                reference.child(key).setValue(q).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        binding.progressBarQuestionForum.setVisibility(View.GONE);
+                        Intent toHome = new Intent(getApplicationContext(), UserHome.class);
+                        toHome.putExtra("uid",uid);
+                        startActivity(toHome);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(AddQuestionForum.this, "Question gagal di post", Toast.LENGTH_SHORT).show();
+                        binding.progressBarQuestionForum.setVisibility(View.GONE);
+                    }
+                });
+            }
+
+        });
+
+        binding.btnBackUserHome.setOnClickListener(view -> {
+            Intent toHome = new Intent(getApplicationContext(), UserHome.class);
+            toHome.putExtra("uid", uid);
+            startActivity(toHome);
         });
     }
 }
